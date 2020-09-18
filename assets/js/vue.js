d506ae718bd4c24776ad03af37af742b85c5f684
adnan50 <adnan.arif50@gmail.com>
2020-09-18 21:54:58 +0500

    code update

---
+++ assets/js/vue.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+  	base_url:"http://localhost/igiplanner",
+    logo: 'Max follower',
+    bannerHeading:"Max Follower is",
+    bannerSubheading:"The best solution for you & ready for more purposes",
+    bannerDescription:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    bannerPhonetext:"QUESTIONS? TEXT US:",
+    phoneNumber:"+1(234)5678910",
+
+
+  }
+})
